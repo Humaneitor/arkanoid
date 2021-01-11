526b8f852c37753a51e0efbbad9825669179fdf8
Ismael Bonilla <ibonilla@DESKTOP-KE9GC80.BAOPARTN.COM>
2021-01-11 21:28:47 +0100

    Primeros pasos

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame as pg
+
+class Game:
+    def __init__(self):
+        self.pantalla = pg.display.set_mode((800, 600))
+        pg.display.set_caption("Futuro Arkanoid")
+        
+
+
+
+if __name__ == '__main__':
+    pg.init()
+    
+
+
